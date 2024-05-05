ca340bd3fde3edf227b125270d963c2e1edf0b61
Reynald Endaya UPOU <rlendaya@up.edu.ph>
2024-05-05 18:47:42 +0800

    fixed JS for contact us form validation

--- customer_page/script.js
+++ customer_page/script.js
@@ -1,5 +1,20 @@
-function sendMessage() {
-  const replaceContainer = document.getElementById("messageContainer");
+const replaceContainer = document.getElementById("messageContainer");
+
+// event listener for form submission
+replaceContainer.addEventListener("submit", function (event) {
+  //  prevent default form submission
+  event.preventDefault();
+
+  // Get the value of the email variables
+  var emailInput = document.getElementById("email").value;
+  var messageInput = document.getElementById("message").value;
+  var nameInput = document.getElementById("name").value;
+
+  // Condition to check if email and message is not blank
+  if (emailInput === "" || nameInput === "" || messageInput === "") {
+    alert("Please fill in all fields!");
+    return;
+  }
 
   const thankYou = document.createElement("p");
   thankYou.textContent =
@@ -10,4 +25,4 @@ function sendMessage() {
   replaceContainer.style.fontWeight = "bold";
   replaceContainer.style.fontSize = "1.5rem";
   replaceContainer.style.marginTop = "1.5rem";
-}
+});
